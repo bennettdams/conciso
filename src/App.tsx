2ce58473b21f34976cca7146b71f608926f47ad3
unknown <dams.bennett@gmail.com>
2019-04-29 23:44:31 +0200

    Add Bulma, Home, Footer, Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
 import React from "react";
 import "./App.scss";
+// import "bulma";
+import "./ui/styles/global.scss";
+
+import Home from "./ui/pages/Home";
+import Navbar from "./ui/components/navbar/Navbar";
+import Footer from "./ui/components/footer/Footer";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>CONCISO</h1>
-      </header>
+      <section>
+        <Navbar />
+      </section>
+      {/*  */}
+      <section className="hero">
+        <Home />
+      </section>
+      {/*  */}
+      <section className="section">
+        <div className="container">
+          <div className="field">
+            <div className="control">
+              <input className="input" type="text" placeholder="Input" />
+            </div>
+          </div>
+
+          <div className="field">
+            <p className="control">
+              <span className="select">
+                <select>
+                  <option>Select dropdown</option>
+                </select>
+              </span>
+            </p>
+          </div>
+
+          <div className="buttons">
+            <a className="button is-primary">Primary</a>
+            <a className="button is-link">Link</a>
+          </div>
+        </div>
+      </section>
+      {/*  */}
+      <section>
+        <Footer />
+      </section>
     </div>
   );
 };
---
+++ src/ui/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="hero-body">
+      <div className="container">
+        <h1 className="title is-size-1 is-family-secondary">CONCISO</h1>
+        <h2 className="subtitle is-size-3 is-italic is-family-primary">
+          INFORMATION THAT HONORS YOUR TIME.
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
